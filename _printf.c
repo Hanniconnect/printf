2bc7c51103bcff84da533d0e26f95ba3497a368f
Ayomide <64165980+Ayomide0123@users.noreply.github.com>
2022-10-19 00:05:20 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,19 +5,21 @@
 * @format: The string to be printed
 * Return: The length of the string
 */
+
 int _printf(const char *format, ...)
 {
-    conver_t fmt[] = {
-        {"c", print_char},
-        {"s", print_string},
-        {"%", print_percent},
-        {"d", print_integer},
-        {"i", print_integer},
-        {NULL, NULL}};
-    int count;
-    va_list spec_list;
-    va_start(spec_list, format);
-    count = parser(format, spec_list, fmt);
-    va_end(spec_list);
-    return (count);
+conver_t fmt[] = {
+{"c", print_char},
+{"s", print_string},
+{"%", print_percent},
+{"d", print_integer},
+{"i", print_integer},
+{NULL, NULL}};
+    
+int count;
+va_list spec_list;
+va_start(spec_list, format);
+count = parser(format, spec_list, fmt);
+va_end(spec_list);
+return (count);
 }
