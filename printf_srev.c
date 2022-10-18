f7d738fd82295b30acda9ee79d97f351c47b7cb4
Ayomide <64165980+Ayomide0123@users.noreply.github.com>
2022-10-18 20:55:25 +0100

    reversed_string

--- printf_srev.c
+++ printf_srev.c
@@ -9,15 +9,15 @@
 int printf_srev(va_list args)
 {
 
-	char *s = va_arg(args, char *);
+	char *r = va_arg(args, char *);
 	int i;
 	int j = 0;
 
-	if (s == NULL)
-		s = "(null)";
-	while (s[j] != '\0')
+	if (r == NULL)
+		r = "(null)";
+	while (r[j] != '\0')
 		j++;
 	for (i = j - 1; i >= 0; i--)
-		_putchar(s[i]);
+		_putchar(r[i]);
 	return (j);
 }
