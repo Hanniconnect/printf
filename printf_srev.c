9bebdc5a28b0d88ef6981c28b914283ad87f77b0
Ayomide <64165980+Ayomide0123@users.noreply.github.com>
2022-10-18 20:59:27 +0100

    reversed_string

--- printf_srev.c
+++ printf_srev.c
@@ -1,23 +1,25 @@
+#include <stdarg.h>
+#include <stdio.h>
 #include "main.h"
-
 /**
- * printf_srev - function that prints a str in reverse
- * @args: type struct va_arg where is allocated printf arguments
- *
- * Return: the string
- */
-int printf_srev(va_list args)
+*print_rev - prints astring in reverse
+*@r: string to print
+*Return: number of chars printed
+*/
+int print_rev(va_list r)
 {
+	char *st;
+	int i, j = 0;
 
-	char *r = va_arg(args, char *);
-	int i;
-	int j = 0;
-
-	if (r == NULL)
-		r = "(null)";
-	while (r[j] != '\0')
+	st = va_arg(r, char *);
+	if (st == NULL)
+		st = ")llun(";
+	for (i = 0; st[i] != '\0'; i++)
+		;
+	for (i -= 1 ; i >= 0; i--)
+	{
+		_putchar(st[i]);
 		j++;
-	for (i = j - 1; i >= 0; i--)
-		_putchar(r[i]);
+	}
 	return (j);
 }
